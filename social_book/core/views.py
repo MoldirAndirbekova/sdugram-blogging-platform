ee810598198b652f29f2b344a3a017705e3f4343
Alisher <recker2014brr@gmail.com>
2023-05-08 00:28:45 +0600

    sample commit

---
+++ social_book/core/views.py
@@ -0,0 +1,119 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile, Post, LikePost
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+@login_required(login_url='signin')
+def index(request):
+    posts = Post.objects.all()
+    user_objects = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_objects)
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        passwordtwo = request.POST['passwordtwo']
+        if password == passwordtwo:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'The email is already used')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'The username has already exists!')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
+        else:
+            messages.info(request, 'Password is not matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Login or Password is invalid')
+            return redirect('signin')
+
+    return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        about = request.POST['about']
+        location = request.POST['location']
+        if request.FILES.get('img') is None:
+            user_profile.bio = about
+            user_profile.location = location
+            user_profile.save()
+            messages.info(request, 'Profile has been updated successfully!')
+        elif request.FILES.get('img') is not None:
+            profile_img = request.FILES.get('img')
+            user_profile.profileImg = profile_img
+            user_profile.bio = about
+            user_profile.location = location
+            user_profile.save()
+            messages.info(request, 'Profile has been updated successfully!')
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
+def upload(request):
+    if request.method == 'POST':
+        user_objects = request.user.username
+        user_image = request.FILES.get('image_upload')
+        user_caption = request.POST['caption']
+        user_post = Post.objects.create(user=user_objects, image=user_image, caption=user_caption)
+        user_post.save()
+        return redirect('/')
+    else:
+        return redirect('')
+    return HttpResponse('<h1>Upload view</h1>')
+
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+    post = Post.objects.get(id=post_id)
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+    if like_filter is None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.number_of_likes += 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.number_of_likes -= 1
+        post.save()
+        return redirect('/')
