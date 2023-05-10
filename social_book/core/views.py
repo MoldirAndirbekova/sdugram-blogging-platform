d7cdfdb67ca1eae23166c4a3ea3ce91a4d2bca23
Moldir <moldir.andirbekova@mail.ru>
2023-05-11 02:03:38 +0600

    "finished first"

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -170,3 +170,20 @@ def search(request):
     return render(request, 'search.html',
                   {'user_profile': user_profile, 'username_profile_list': username_profile_list})
 
+
+@login_required(login_url='signin')
+def follow(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
+    else:
+        return redirect('/')
