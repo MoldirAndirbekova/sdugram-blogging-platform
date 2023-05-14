768ff1a0c16ae92d0ffe697648c65ff9adf39540
welray17 <temirlan2003@list.ru>
2023-05-14 20:10:37 +0600

    1

--- social_book/core/admin.py
+++ social_book/core/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-
-
-from .models import Profile, Post, LikePost, Comment
-from . import models
-
-from .models import Profile, Post, LikePost, FollowersCount
-
-from .models import Profile, Post, LikePost, Comment
-from . import models
 from .models import Profile, Post, LikePost, FollowersCount,Comment
 
 
